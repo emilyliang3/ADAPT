8adda071cdb787fa77add01983208d7d002aa18f
coortiz1 <105026533+coortiz1@users.noreply.github.com>
2023-05-08 14:25:32 -0700

    added new textbox functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,26 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from 'react';
+export default function MyForm() {
+  const [name, setName] = useState("");
+  const [displayName,setDisplayName] = useState("");
+
+  function handleSubmit(event){
+    event.preventDefault();
+    setDisplayName(name);
+  }
+
+  return (
+    <>
+      <h1>{displayName}</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+          <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
