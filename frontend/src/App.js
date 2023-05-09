ca7e02e4f5cdbc022d5448f4d4a97c413aa29f57
coortiz1 <105026533+coortiz1@users.noreply.github.com>
2023-05-08 18:52:17 -0700

    added google button
    google button that runs handleClick when clicked; also minor aesthetic changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,8 @@
 import { useState } from 'react';
-const user = {
-  firstName:"curtis",
-  lastName:"chen",
-  weight:"50",
-  gender:"male"
-};
+function handleClick(){
 
-function User({user}) {
-  return (
-    <>
-      <h2>Name is {user.firstName}</h2>
-      <h2>Weight is {user.weight}</h2>
-      <h2>Gender is {user.gender}</h2>
-    </>
-  );
 }
+
 function MyForm({question,changeValue}) {
   const [name, setName] = useState("");
 
@@ -41,16 +29,24 @@ function MyForm({question,changeValue}) {
 export default function App(){
   const [userName,setUserName] = useState("");
   const [password,setPassword] = useState("");
+  const [weight,setWeight] = useState("");
   return (
   <>
     <h1> ADAPT (Aiding Dietician and Personal Trainer)</h1>
+    <h2>Google Signin:</h2>
+    <button onClick = {handleClick}>Sign in with Google</button>
+
+    <h2>Manual Signin:</h2>
     <MyForm question = "Username: " changeValue = {setUserName}/>
     <MyForm question = "Password: " changeValue = {setPassword}/>
     <h3>Your username is {userName}</h3>
     <h3>Your password is {password}</h3>
-    <User user = {user}/>
+    <h2>Your Information:</h2>
+    <MyForm question = "Weight: " changeValue = {setWeight}/>
+
+    <h3>Your weight is {weight}</h3>
+
   </>
   );
-}
 
-  
+}
