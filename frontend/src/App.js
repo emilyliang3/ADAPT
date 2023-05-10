7dac8de67192a4ef4101a4bbdf70b5a7d6522643
YuYuan-Chang <lawani321@gmail.com>
2023-05-09 21:55:53 -0700

    Merge with Curtis, No auth.js, switch firebase key ID

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,81 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Auth } from './auth';
-import {db} from './firebase'
-import { getDocs, collection } from 'firebase/firestore' 
+import {auth, googleProvider} from "./firebase";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
 
-function App() {
-  const [movieList, setMovieList]  = useState([])
-  const moviesCollectionRef = collection(db, "movies")
+function MyForm({question,changeValue}) {
+  const [name, setName] = useState("");
 
-  useEffect(() => {
-    const getMovieList = async () => {
-      //Read the data
-      //set the movie list
-      try {
-        const data = await getDocs(moviesCollectionRef);
-      } catch (err) {
-        console.error(err);
-      }
-  };
-    getMovieList();
-  }, [])
+  function handleSubmit(event){
+    event.preventDefault();
+    changeValue(name);
+    setName("");
+  }
 
-  
   return (
-    <div className="App">
-    <Auth /> 
-    
-    <button></button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>{question} 
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
   );
 }
 
-export default App;
+export default function App(){
+    const [email, setemail] = useState("");
+    const [password, setPassword] = useState("");
+    const [weight,setWeight] = useState("");
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+          } catch (err) {
+            console.error(err);
+          }
+    };
+
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+          } catch (err) {
+            console.error(err);
+          }
+    };
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
+          } catch (err) {
+            console.error(err);
+          }
+    };
+
+    return (
+    <div>
+      <h1> ADAPT (Aiding Dietician and Personal Trainer)</h1>     
+      <input 
+      placeholder="Email..."
+      onChange = {(e) => setemail(e.target.value)}
+      />
+      
+      <input 
+      placeholder="Password..."
+      type = "password"
+      onChange = {(e) => setPassword(e.target.value)}
+      />
+      
+      <h2>Manual Signin:</h2>
+      <button onClick={signIn}> Sign In</button>
+
+      <h2>Google Signin:</h2>
+      <button onClick={signInWithGoogle}> Sign In With Google</button>
+      <button onClick={logout}> Logout </button>
+
+      <h2>Your Information:</h2>
+      <MyForm question = "Weight: " changeValue = {setWeight}/>
+    </div>
+    );
+};
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,13 +9,13 @@ import {GoogleAuthProvider, getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgonR9Q1aky2wyOCbkpbV-VK_lm8e3L4U",
-  authDomain: "adapt-ee5b3.firebaseapp.com",
-  projectId: "adapt-ee5b3",
-  storageBucket: "adapt-ee5b3.appspot.com",
-  messagingSenderId: "493689765468",
-  appId: "1:493689765468:web:0656f64296ef7e6d1d3099",
-  measurementId: "G-SZJPGXT8JP"
+    apiKey: "AIzaSyBgonR9Q1aky2wyOCbkpbV-VK_lm8e3L4U",
+    authDomain: "adapt-ee5b3.firebaseapp.com",
+    projectId: "adapt-ee5b3",
+    storageBucket: "adapt-ee5b3.appspot.com",
+    messagingSenderId: "493689765468",
+    appId: "1:493689765468:web:0656f64296ef7e6d1d3099",
+    measurementId: "G-SZJPGXT8JP"
 };
 
 // Initialize Firebase
