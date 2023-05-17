3b206a700f7f945ecfb3fa9696d2ab7972e3dc3f
Curtis Chen <curtischen1234@gmail.com>
2023-05-16 21:27:57 -0700

    Added new prompts for name and birthdate in UserInfo.js

--- frontend/src/pages/UserInfo.js
+++ frontend/src/pages/UserInfo.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 import { updateWeight } from '../userFunctions.js';
 
 function UserInfo() {
+    const [name, setName] = useState("")
     const [weight,setWeight] = useState("");
+    const [birthMonth,setBirthMonth] = useState("");
+    const [birthDay,setBirthDay] = useState("");
+    const [birthYear,setBirthYear] = useState("");
+
 
     function changeValue(weight) {
         setWeight(weight);
@@ -12,10 +17,16 @@ function UserInfo() {
 
     return (
         <>
+            <h1>Hi {name}!</h1>
+            <MyForm question = "Name: " changeValue = { changeValue }/>
             <h2>Your Information:</h2>
             <MyForm question = "Weight: " changeValue = { changeValue }/>
             <h4>Your weight is {weight}</h4>
-            <MyForm question = "Weight: " changeValue = { changeValue }/>
+            <MyForm question = "Birth Month (1-12): " changeValue = { changeValue }/>
+            <MyForm question = "Birth Day (1-31): " changeValue = { changeValue }/>
+            <MyForm question = "Birth Year (1900-2023): " changeValue = { changeValue }/>
+            <h4>{birthMonth} / {birthDay} / {birthYear}</h4>
+            
         </>
     );
 }
