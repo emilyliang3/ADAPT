80e4f16f8dde7bf7044939f0e487637a07b20fef
Curtis Chen <curtischen1234@gmail.com>
2023-05-16 22:57:34 -0700

    Add images to workout.js

--- frontend/src/pages/Workout.js
+++ frontend/src/pages/Workout.js
@@ -1,6 +1,18 @@
+import workout1 from '../images/workout1.jpg';
+import workout2 from '../images/workout2.jpg';
+import workout3 from '../images/workout3.jpg';
+
+import food1 from '../images/food1.jpg';
+
+
 function Workout() {
     return (
-        <h1>this is the workout</h1>
+        <>
+            <h1>this is the workout</h1>
+            <img src = {workout1} className = "picture"/>
+            <img src = {workout2} className = "picture"/>
+            <img src = {workout3} className = "picture"/>
+        </>
     );
 }
 
