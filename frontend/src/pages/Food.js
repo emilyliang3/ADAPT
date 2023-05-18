3c9f97ad9b2b302dd8c860cee05403b51b58a937
Curtis Chen <curtischen1234@gmail.com>
2023-05-17 22:09:05 -0700

    Added function DisplayOneRecipe
    This function displays the attributes of a recipe.

--- frontend/src/pages/Food.js
+++ frontend/src/pages/Food.js
@@ -1,7 +1,7 @@
 import food1 from '../images/food1.jpg';
 import food2 from '../images/food2.jpg';
 import food3 from '../images/food3.jpg';
-const Apple = ["Apple","fruit","95"]
+const Apple = ["Apple","fruit","95"];
 function Food() {
     function Food({arr}) {
         return (
@@ -12,13 +12,34 @@ function Food() {
                 <li key={index}>{item}</li>
               ))}
             </ol>
+            
           </div>
         );
+    }
+
+    function DisplayOneRecipe({recipeName}){
+      const name = "taco";
+      const nutritionalInfo = "none";
+      const ingredients = ["salt","sugar"];
+      const tags = ["high protein", "dairy free"];
+      return (
+        <>
+          <h3>Our recipe makes a {name}</h3>
+          <h3>Nutritional Info: {nutritionalInfo}</h3>
+          <h3>Ingredients</h3>
+          <ol>
+            {ingredients.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ol>                    
+        </>
+      );
     }
     return (
         <>
             <h1>this is the food</h1>
             <Food arr = {Apple} />
+            <DisplayOneRecipe />
             <img src = {food1} className = "picture"/>
             <img src = {food2} className = "picture"/>
             <img src = {food3} className = "picture"/>
