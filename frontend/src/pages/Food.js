432daf00ab6fcb98bacd222e6adf95264c698665
emilyliang3 <59712728+emilyliang3@users.noreply.github.com>
2023-05-17 22:54:28 -0700

    incorporated getRecipeData() into Food.js
    Recipe info is now displayed on the food page. Currently the recipe is hard coded in.

--- frontend/src/pages/Food.js
+++ frontend/src/pages/Food.js
@@ -1,45 +1,63 @@
 import food1 from '../images/food1.jpg';
 import food2 from '../images/food2.jpg';
 import food3 from '../images/food3.jpg';
-const Apple = ["Apple","fruit","95"];
+import { useState } from "react";
+import { getRecipeData } from '../recipeFunctions';
+
 function Food() {
-    function Food({arr}) {
-        return (
-          <div>
-            <h1>This is the food</h1>
-            <ol>
-              {arr.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ol>
-            
-          </div>
-        );
+  function DisplayOneRecipe({recipeName}){
+    const [name, setName] = useState("N/A");
+    const [calories, setCalories] = useState("N/A");
+    const [fat, setFat] = useState("N/A");
+    const [protein, setProtein] = useState("N/A");
+    const [ingredients, setIngredients] = useState([]);
+    const [instructions, setInstructions] = useState("N/A");
+
+    getRecipeData(recipeName).then((obj) => {
+      if (obj.name) {
+        setName(obj.name);
+      }
+      if (obj.calories) {
+        setCalories(obj.calories);
+      }
+      if (obj.fat) {
+        setFat(obj.fat);
+      }
+      if (obj.protein) {
+        setProtein(obj.protein);
+      }
+      if (obj.ingredients) {
+        setIngredients(obj.ingredients);
+      }
+      if (obj.instructions) {
+        setInstructions(obj.instructions);
+      }
+    });
+
+    return (
+      <>
+        <h3>Recipe: {name}</h3>
+        <h3>Nutritional Info (per serving):</h3>
+        <h4>Calories: {calories}</h4>
+        <h4>Fat &#40;grams&#41;: {fat}</h4>
+        <h4>Protein &#40;grams&#41;: {protein}</h4>
+        <h3>Ingredients: </h3>
+        <ol>
+          {ingredients.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol> 
+        <a href={instructions}>Click here for instructions and full recipe</a>                   
+      </>
+    );
     }
 
-    function DisplayOneRecipe({recipeName}){
-      const name = "taco";
-      const nutritionalInfo = "none";
-      const ingredients = ["salt","sugar"];
-      const tags = ["high protein", "dairy free"];
-      return (
-        <>
-          <h3>Our recipe makes a {name}</h3>
-          <h3>Nutritional Info: {nutritionalInfo}</h3>
-          <h3>Ingredients</h3>
-          <ol>
-            {ingredients.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ol>                    
-        </>
-      );
-    }
     return (
         <>
-            <h1>this is the food</h1>
-            <Food arr = {Apple} />
-            <DisplayOneRecipe />
+            <h1>Recipe Search</h1>
+            <h3>search gui will eventually be here...</h3>
+            <h2>Here are some recipes that fit your criteria!</h2>
+            <DisplayOneRecipe recipeName = "PeanutButterBananaPancakes" />
             <img src = {food1} className = "picture"/>
             <img src = {food2} className = "picture"/>
             <img src = {food3} className = "picture"/>
