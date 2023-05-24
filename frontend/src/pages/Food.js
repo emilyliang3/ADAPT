437df480c02a279546b9c5465a6c218d6f1ed165
Curtis Chen <curtischen1234@gmail.com>
2023-05-24 11:45:17 -0700

    Add option to display multiple recipes by taking in an array of food names

--- frontend/src/pages/Food.js
+++ frontend/src/pages/Food.js
@@ -3,7 +3,21 @@ import food2 from '../images/food2.jpg';
 import food3 from '../images/food3.jpg';
 import {useState} from 'react';
 const Apple = ["Apple","fruit","95"];
-function MyForm() {
+
+function Food() {
+  function Food({arr}) {
+      return (
+        <div>
+          <h1>This is the food</h1>
+          <ol>
+            {arr.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ol>
+            
+        </div>
+      );
+  }
   const [checkboxValues, setCheckboxValues] = useState({
     highProtein: false,
     lowFat: false,
@@ -12,152 +26,152 @@ function MyForm() {
     glutenFree: false,
     vegetarian: false,
   });
+  function MyForm() {
+    const handleCheckboxChange = (event) => {
+      const { name, checked } = event.target;
+      setCheckboxValues((prevState) => ({
+        ...prevState,
+        [name]: checked
+      }));
+    };
 
-  const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;
-    setCheckboxValues((prevState) => ({
-      ...prevState,
-      [name]: checked
-    }));
-  };
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Handle form submission or perform any desired actions with checkboxValues
+      console.log(checkboxValues);
+      setCheckboxValues({
+        highProtein: false,
+        lowFat: false,
+        lowCalorie: false,
+        dairyFree: false,
+        glutenFree: false,
+        vegetarian: false,
+      });
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission or perform any desired actions with checkboxValues
-    console.log(checkboxValues);
-    setCheckboxValues({
-      highProtein: false,
-      lowFat: false,
-      lowCalorie: false,
-      dairyFree: false,
-      glutenFree: false,
-      vegetarian: false,
-    });
-  };
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="checkbox"
+            name="highProtein"
+            checked={checkboxValues.highProtein}
+            onChange={handleCheckboxChange}
+          />
+          High Protein
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="lowFat"
+            checked={checkboxValues.lowFat}
+            onChange={handleCheckboxChange}
+          />
+          Low Fat
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="lowCalorie"
+            checked={checkboxValues.lowCalorie}
+            onChange={handleCheckboxChange}
+          />
+          Low Calorie
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="dairyFree"
+            checked={checkboxValues.dairyFree}
+            onChange={handleCheckboxChange}
+          />
+          Dairy Free
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="glutenFree"
+            checked={checkboxValues.glutenFree}
+            onChange={handleCheckboxChange}
+          />
+          Gluten Free
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="vegetarian"
+            checked={checkboxValues.vegetarian}
+            onChange={handleCheckboxChange}
+          />
+          Vegetarian
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <input
-          type="checkbox"
-          name="highProtein"
-          checked={checkboxValues.highProtein}
-          onChange={handleCheckboxChange}
-        />
-        High Protein
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="lowFat"
-          checked={checkboxValues.lowFat}
-          onChange={handleCheckboxChange}
-        />
-        Low Fat
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="lowCalorie"
-          checked={checkboxValues.lowCalorie}
-          onChange={handleCheckboxChange}
-        />
-        Low Calorie
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="dairyFree"
-          checked={checkboxValues.dairyFree}
-          onChange={handleCheckboxChange}
-        />
-        Dairy Free
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="glutenFree"
-          checked={checkboxValues.glutenFree}
-          onChange={handleCheckboxChange}
-        />
-        Gluten Free
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="vegetarian"
-          checked={checkboxValues.vegetarian}
-          onChange={handleCheckboxChange}
-        />
-        Vegetarian
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-function Food() {
-    function Food({arr}) {
-        return (
-          <div>
-            <h1>This is the food</h1>
-            <ol>
-              {arr.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ol>
-             
-          </div>
-        );
-    }
-
-    function DisplayOneRecipe({recipeName}){
-      const name = "taco";
-      const nutritionalInfo = "none";
-      const ingredients = ["salt","sugar"];
-      const df = true;
-      const gf = true;
-      const veg = true;
-      function Tag({tagName,tf}){
-        if (tf)
-          return <h4>{tagName}</h4>;
-        return;
-      }
-      return (
-        <>
-          <h3>Our recipe makes a {name}</h3>
-          <h3>Nutritional Info: {nutritionalInfo}</h3>
-          <h3>Ingredients</h3>
-          <ol>
-            {ingredients.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ol>
-          <h3>Tags</h3> 
-          <Tag tagName = 'DF' tf = {df} />
-          <Tag tagName = 'GF' tf = {gf} />
-          <Tag tagName = 'VEG' tf = {veg} />                  
-        </>
-      );
+  function DisplayOneRecipe({recipeName}){
+    const name = "taco";
+    const nutritionalInfo = "none";
+    const ingredients = ["salt","sugar"];
+    const df = true;
+    const gf = true;
+    const veg = true;
+    function Tag({tagName,tf}){
+      if (tf)
+        return <h4>{tagName}</h4>;
+      return;
     }
     return (
       <>
-        <MyForm />
-        <h1>this is the food</h1>
-        <button type="Sort by calories">Sort by calories</button>
-        <button type="Sort by protein">Sort by protein</button>
-        <Food arr = {Apple} />
-        <DisplayOneRecipe />
-        <img src = {food1} className = "picture"/>
-        <img src = {food2} className = "picture"/>
-        <img src = {food3} className = "picture"/>
+        <h3>Our recipe makes a {name}</h3>
+        <h3>Nutritional Info: {nutritionalInfo}</h3>
+        <h3>Ingredients</h3>
+        <ol>
+          {ingredients.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+        <h3>Tags</h3> 
+        <Tag tagName = 'DF' tf = {df} />
+        <Tag tagName = 'GF' tf = {gf} />
+        <Tag tagName = 'VEG' tf = {veg} />                  
       </>
     );
+  }
+
+  function DisplayMultiple({test}){
+    return (
+      <ol>
+          {test.map((item, index) => (
+            <li key={index}>
+              <DisplayOneRecipe recipleName = {item} />
+            </li>
+          ))}
+      </ol>
+    )
+  }
+  const test = ["food1","food2","food3"];
+  return (
+    <>
+      <MyForm />
+      <h1>this is the food</h1>
+      <button type="Sort by calories">Sort by calories</button>
+      <button type="Sort by protein">Sort by protein</button>
+      <Food arr = {Apple} />
+      <DisplayOneRecipe />
+      <img src = {food1} className = "picture"/>
+      <img src = {food2} className = "picture"/>
+      <img src = {food3} className = "picture"/>
+      <DisplayMultiple test = {test} />
+    </>
+  );
 }
 
 export default Food;
