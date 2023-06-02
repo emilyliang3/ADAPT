c86ea1fa39947bc24a0dbfee92758d13e8c4a4d1
YuYuan-Chang <lawani321@gmail.com>
2023-06-01 21:08:21 -0700

    fix login sec

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -39,28 +39,22 @@ export default function LoginButtons(){
       <h3 className="message">Welcome to ADAPT!<br></br>Visit the Food page to find recipes tailored to specific nutritional goals and/or needs.<br></br>Vist the Workout page to search for workouts that align with your fitness goals. </h3>
       <h3 className="blue">{welcomeMessage}</h3>
 
-      <h2>Existing users:</h2> 
-      <input className="field" placeholder="Email..."
-      onChange = {(e) => setemail(e.target.value)}
-      />
-      <input className="field" placeholder="Password..."
-      onChange = {(e) => setPassword(e.target.value)}
-      />
-      <button className="sign" onClick={() => signInWithEmail(email, password)}>Sign In</button>
-      <h3 className="or">or</h3>
-      <button className="google" onClick={() => signInWithGoogle()}> Sign In with Google</button>
-        <div className="bigspace"></div>
-
+      
       <div className = "color">
 
         <div className = "box2">
           <h2>Existing users:</h2> 
-          <input className="field" placeholder="Email..."></input>
-          <input className="field" placeholder="Password..."></input>
-          <button className="sign">Sign In</button>
+          <input className="field" placeholder="Email..."
+          onChange = {(e) => setemail(e.target.value)}
+          />
+          <input className="field" placeholder="Password..."
+          onChange = {(e) => setPassword(e.target.value)}
+          />
+          <button className="sign" onClick={() => signInWithEmail(email, password)}>Sign In</button>
           <h3 className="or">or</h3>
           <button className="google" onClick={() => signInWithGoogle()}> Sign In with Google</button>
             <div className="bigspace"></div>
+
 
           <h2>Create an account:</h2>
           <input className="field" 
